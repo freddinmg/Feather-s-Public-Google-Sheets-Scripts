07073d5b7dcf4957da4663acc642f5c3a350e548
freddinmg <79553156+freddinmg@users.noreply.github.com>
2024-01-02 07:33:46 -0600

    Create getSheetURLAndId.js
    This code is for getting the Changelog link. Keep in mind that you should NOT run this script, just have it in the project. If you use it from here, it'll grab the URL of "Front Page."

---
+++ getSheetURLAndId.js
@@ -0,0 +1,36 @@
+/* @OnlyCurrentDoc */
+
+/*function getSheetUrlAndId1() {
+  var SS = SpreadsheetApp.getActiveSpreadsheet();
+  var ss = SS.getActiveSheet();
+  var url = '';
+  url += SS.getUrl();
+  url += '#gid=';
+  url += ss.getSheetId(); 
+  return url;
+}
+/*
+/* @OnlyCurrentDoc */
+function getSheetUrlAndId2() {
+  var SS = SpreadsheetApp.getActiveSpreadsheet();
+  var ss = SS.getActiveSheet();
+  var url3 = '';
+  url3 += SS.getUrl();
+  url3 += '#gid=';
+  url3 += ss.getSheetId(); 
+  url3 += '&range=I'; 
+  return url3;
+}
+
+//DO NOT RUN THIS, RUN THE NEXT PAGE
+function linkOnSpreadsheet () {
+  var sheet3 = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Changelog');
+  var sheet4 = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Front Page');
+  var cellValue2 = getSheetUrlAndId2();
+  sheet3.getRange("A2").setValue('=hyperlink("'+cellValue2+'"& COUNTA(I3:I) - 5,"To Latest")');
+  sheet4.getRange("GrabbedLink2").setValue(cellValue2);
+}
+
+
+
+/* @OnlyCurrentDoc */
